dba51a5c794bfa2bbc308f0d06af2e93d766c0df
root <jinwei.zeng@hotmail.com>
2024-08-03 08:41:42 +0800

    update diy scripts.

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Update theme argonv3
-#rm -rf feeds/luci/themes/luci-theme-argonv3
-#git clone https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argonv3
-#
-## Update theme argonv3
-#rm -rf feeds/luci/themes/luci-theme-design
-#git clone https://github.com/0x676e67/luci-theme-design feeds/luci/themes/luci-theme-design
-#
-## Change Default Design
-#sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
-#sed -i 's/Bootstrap/Design/g' feeds/luci/collections/luci/Makefile
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
