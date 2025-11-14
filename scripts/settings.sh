cf982a68fabd6b057f57259b75fb11cef3e53258
Jinwei Zeng <90731642+jinwei91@users.noreply.github.com>
2025-11-14 17:18:54 +0800

    Update settings.sh

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -18,17 +18,3 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 # Modify Hostname
 sed -i 's/LiBwrt/OpenWrt/g' package/base-files/files/bin/config_generate
-
-# Enable Cache
-#echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
-
-# 修改默认IP & 固件名称 & 编译署名
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-#sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_generate
-#sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ Built by Roc')/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
-
-# 调整NSS驱动q6_region内存区域预留大小（ipq6018.dtsi默认预留85MB，ipq6018-512m.dtsi默认预留55MB，带WiFi必须至少预留54MB，以下分别是改成预留16MB、32MB、64MB和96MB）
-# sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x01000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
-# sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x02000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
-# sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x04000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
-# sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x06000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dts
