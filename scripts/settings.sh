600d043032465686feb63d0695e31f69b53fd4ba
Jinwei Zeng <90731642+jinwei91@users.noreply.github.com>
2025-11-14 17:11:51 +0800

    Update settings.sh

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -10,103 +10,25 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
+# Modify Default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# Modify Default Theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# Modify Hostname
+sed -i 's/LiBwrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # Enable Cache
-echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
+#echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
 
 # 修改默认IP & 固件名称 & 编译署名
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_generate
-sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ Built by Roc')/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
-
-# 调整在Argon主题下，概览页面显示/隐藏按钮的样式
-sed -i '/^\.td\.cbi-section-actions {$/,/^}$/ {
-    /^}$/a\
-.cbi-section.fade-in .cbi-title {\
-  position: relative;\
-  min-height: 2.765rem;\
-  display: flex;\
-  align-items: center\
-}\
-.cbi-section.fade-in .cbi-title>div:last-child {\
-  position: absolute;\
-  right: 1rem\
-}\
-.cbi-section.fade-in .cbi-title>div:last-child span {\
-  display: inline-block;\
-  position: relative;\
-  font-size: 0\
-}\
-.cbi-section.fade-in .cbi-title>div:last-child span::after {\
-  content: "\\e90f";\
-  font-family: '\''argon'\'' !important;\
-  font-size: 1.1rem;\
-  display: inline-block;\
-  transition: transform 0.3s ease;\
-  -webkit-font-smoothing: antialiased;\
-  line-height: 1\
-}\
-.cbi-section.fade-in .cbi-title>div:last-child span[data-style='\''inactive'\'']::after {\
-  transform: rotate(90deg);\
-}
-}' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-
-sed -i -e '/btn\.setAttribute(\x27class\x27, include\.hide ? \x27label notice\x27 : \x27label\x27);/d' \
-      -e "/\x27class\x27: includes\[i\]\.hide ? \x27label notice\x27 : \x27label\x27,/d" \
-         feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/index.js
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_generate
+#sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ Built by Roc')/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 
 # 调整NSS驱动q6_region内存区域预留大小（ipq6018.dtsi默认预留85MB，ipq6018-512m.dtsi默认预留55MB，带WiFi必须至少预留54MB，以下分别是改成预留16MB、32MB、64MB和96MB）
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x01000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x02000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x04000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
-# sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x06000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
-
-# 移除要替换的包
-rm -rf feeds/luci/applications/luci-app-wechatpush
-rm -rf feeds/luci/applications/luci-app-appfilter
-rm -rf feeds/luci/applications/luci-app-frpc
-rm -rf feeds/luci/applications/luci-app-frps
-rm -rf feeds/packages/net/open-app-filter
-rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/ariang
-rm -rf feeds/packages/net/frp
-rm -rf feeds/packages/lang/golang
-
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
-# Go & OpenList & ariang & frp & AdGuardHome & WolPlus & Lucky & wechatpush & OpenAppFilter & 集客无线AC控制器 & 雅典娜LED控制
-git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
-git clone --depth=1 https://github.com/sbwml/luci-app-openlist2 package/openlist
-git_sparse_clone ariang https://github.com/laipeng668/packages net/ariang
-git_sparse_clone frp https://github.com/laipeng668/packages net/frp
-mv -f package/frp feeds/packages/net/frp
-git_sparse_clone frp https://github.com/laipeng668/luci applications/luci-app-frpc applications/luci-app-frps
-mv -f package/luci-app-frpc feeds/luci/applications/luci-app-frpc
-mv -f package/luci-app-frps feeds/luci/applications/luci-app-frps
-git_sparse_clone master https://github.com/kenzok8/openwrt-packages adguardhome luci-app-adguardhome
-git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
-git clone --depth=1 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
-git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
-git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
-chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
-
-./scripts/feeds update -a
-./scripts/feeds install -a
+# sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x06000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dts
